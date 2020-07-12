14ca70b0c093f2abb8d2c7d43ce1ca04c7e31be4
BitVivAZ <jembuxmann@gmail.com>
2020-07-12 17:10:16 +0200

    Updates

---
+++ precache-manifest.7af2ef56af69d62caee1492026e5e9d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10c9ee578af37f0bc1da640fb96453f",
+    "url": "/fare/index.html"
+  },
+  {
+    "revision": "0d7ccdfbfde82dbec9bc",
+    "url": "/fare/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "d2132748908d38c22031",
+    "url": "/fare/static/css/main.6ef35eae.chunk.css"
+  },
+  {
+    "revision": "0d7ccdfbfde82dbec9bc",
+    "url": "/fare/static/js/2.c321a647.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fare/static/js/2.c321a647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2132748908d38c22031",
+    "url": "/fare/static/js/main.37465a3a.chunk.js"
+  },
+  {
+    "revision": "9581f2fda509b8a6fbe4",
+    "url": "/fare/static/js/runtime-main.0a11a94a.js"
+  }
+]);
